62656810f3be7f28e35825aa50420ab0f74f4367
7thCore <the7thcore@gmail.com>
2022-01-27 14:07:08 +0100

    Removed samba from dependancies

--- ausrv-script.bash
+++ ausrv-script.bash
@@ -21,7 +21,7 @@
 
 #Basics
 export NAME="AuSrv" #Name of the tmux session
-export VERSION="1.0-2" #Package and script version
+export VERSION="1.0-3" #Package and script version
 
 #Server configuration
 export SERVICE_NAME="ausrv" #Name of the service files, user, script and script log
